926a461c6a9fe76b48f5ef125dd85530a9d4569c
Sean Despres <despres.sean@gmail.com>
2020-11-18 11:06:06 -0500

    ex2 Bonus2 completed

--- ex2_odd_even.py
+++ ex2_odd_even.py
@@ -5,10 +5,13 @@
 # Bonus1: If the number is divisable by 4 print out a different message
 # Bonus2: Ask for 2 numbers and test to see if they are evenly divisable
 
-user_number = input("Hello, enter a number and I will tell you if it is even or odd: ")
-if int(user_number)%4 == 0:
-    print(f"Wow, you chose {user_number} and it is divisable by 4!")
-elif int(user_number)%2 == 0:
-    print(f"The number {user_number} is even!")
+print("")
+print("Hello, enter a number and a divisor and I will tell you if the number is evenly divided by the divisor!")
+user_number = input("Enter a number: ")
+user_divisor = input("Enter a divisor: ")
+print("")
+if int(user_number)%int(user_divisor) == 0:
+    print(f"Wow, {user_number} is evenly divisable by {user_divisor}, you are amazing!")
 else:
-    print(f"The number {user_number} is odd, just like you!")
+    print(f"Whomp whomp, {user_number} is NOT evenly divisable by {user_divisor}, better luck next time!")
+print("")
