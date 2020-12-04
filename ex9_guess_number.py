684f92900d40de38f5a2025c8ff7b6f9f3e36df1
Sean Despres <despres.sean@gmail.com>
2020-12-04 16:55:35 -0500

    ex9 Bonus Complete

--- ex9_guess_number.py
+++ ex9_guess_number.py
@@ -12,12 +12,28 @@ import random
 random_number = random.randint(1, 11)
 print(random_number)
 
-user_guess = int(input("\nHello! I am thinking of a number between 1 and 10, can you guess what it is? "))
+user_guess = 0
+user_attempt = 0
 
-if user_guess == random_number:
-    print(f"Wow, you are right the number is {random_number}!")
-elif user_guess > random_number:
-    print(f"Sorry, {user_guess} is too high!")
-elif user_guess < random_number:
-    print(f"Sorry, {user_guess} is too low!")    
+print("\nHello! I am thinking of a number between 1 and 10.")
+
+while user_guess != "exit" or int(user_guess) != random_number:
+    if user_attempt == 0:
+        user_guess = input("Can you guess what it is (type exit to quit)? ")
+    else:
+        user_guess = input("\nTry again: ")
+    if user_guess == "exit":
+        print("Thanks for playing!")
+    elif int(user_guess) == random_number:
+        user_attempt+=1
+        print(f"Wow, you are right the number is {random_number} and you guessed it in {user_attempt}!\n")
+        break
+    elif int(user_guess) > random_number:
+        user_attempt+=1
+        print(f"Sorry, {user_guess} is too high!")
+    elif int(user_guess) < random_number:
+        user_attempt+=1
+        print(f"Sorry, {user_guess} is too low!")
+    
+  
 
